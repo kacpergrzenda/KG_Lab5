fba9e4d2a5f7fd218e463673ae2bd00033b19059
Kacper grzenda <kacpergrzenda1@gmail.com>
2020-02-25 17:31:24 +0000

    lab5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SavingsComponent } from './savings/savings.component';
+import { MortgageComponent } from './mortgage/mortgage.component';
+import { FormsModule } from '@angular/forms';
+import { InterestService } from './services/interest.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SavingsComponent,
+    MortgageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [InterestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/savings/savings.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { InterestService } from '../Services/interest.service';
+
+@Component({
+  selector: 'app-savings',
+  templateUrl: './savings.component.html',
+  styleUrls: ['./savings.component.css']
+})
+export class SavingsComponent implements OnInit {
+  constructor(private service: InterestService) { }
+
+  ngOnInit(): void {
+  }
+
+  Total: number = 0;
+  years: number;
+  calculateinterest(): void {
+    this.Total = this.service.calculateinterest(this.years);
+  }
+
+  
+
+}
